0bc2d6f2e20edd6e087c6423c65a4507182cc553
Ward Zidani <86842722+WardZid@users.noreply.github.com>
2022-08-08 20:32:24 +0300

    simple game logic added

--- src/app/gameboard/gameboard.component.ts
+++ src/app/gameboard/gameboard.component.ts
@@ -32,7 +32,23 @@ export class GameboardComponent {
 
   checkWinner():void{
 
-    
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (this.squares[a] !== "" && this.squares[a] === this.squares[b] && this.squares[a] === this.squares[c]) {
+        this.winner=this.squares[a];
+        return;
+      }
+    }
     this.winner="";
   }
 }
