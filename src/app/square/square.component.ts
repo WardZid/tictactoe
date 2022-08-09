ab22c2c39d734d66aadc45e76efd52f00ce73f40
Ward Zidani <86842722+WardZid@users.noreply.github.com>
2022-08-09 13:36:23 +0300

    cosmetics and alignment

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: '<button>{{ value }}</button>',
+  template: ''+
+  '<button mat-raised-button color="primary">{{value}}</button>',
   styleUrls: ['./square.component.sass']
 })
 export class SquareComponent {
